0b4c8834c5682c6fd9b220d22bfc71a21fa05940
Aguidev <aguilera.fede@yahoo.com.ar>
2023-04-02 08:05:22 -0300

    Cambios apikey

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,3 @@
-const apiKey = "7a68273b19f28a33b1ae57a8541b6ec3";
 const api = `https://api.openweathermap.org/data/3.0/onecall?lat={-34.61315}&lon={-58.37723}&exclude={part}&appid=${apiKey}`;
 
 const getClima = async () => {
