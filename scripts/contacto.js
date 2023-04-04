95f9a482b02b893f2a07419c37a5a70a54c22523
RocioBritos3747 <110845474+RocioBritos3747@users.noreply.github.com>
2023-04-04 11:08:07 -0300

    Create contacto.js

---
+++ scripts/contacto.js
@@ -0,0 +1,31 @@
+const usuarios = []
+
+const formularioContacto = document.querySelector(".form")
+const contenedorHTML = document.querySelector("#contenedorUsuarios")
+
+const renderizarUsuarios = () => {
+    contenedorHTML.innerHTML = ""
+    for(const usuario of usuarios){
+        contenedorHTML.innerHTML += `
+        <div class="c-usuario">
+            <h3>Nombre: ${usuario.nombre}</h3>
+            <h3>Apellido: ${usuario.apellido}</h3>
+            <p>Teléfono: <strong>${usuario.telefono}</strong></p>
+            <p>Correo electrónico: <strong>${usuario.email}</strong></p>
+            <p>Comentario: <strong>${usuario.comentario}</strong></p>
+        </div>
+        `
+    }
+}
+
+formularioContacto.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    usuarios.push({
+        nombre: formularioContacto.nombre_ingresado.value, 
+        apellido: formularioContacto.apellido_ingresado.value,
+        telefono: formularioContacto.telefono_ingresado.value,
+        email: formularioContacto.email_ingresado.value,
+        comentario: formularioContacto.comentario.value,
+    })
+    renderizarUsuarios()
+})
